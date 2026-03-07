e67b1226325235c6ba2a95e11b79b1adc7f61a33
saifchima <bnmhmhmd438@gmail.com>
2026-03-07 20:29:42 +0100

    you can lose

--- snake game.py
+++ snake game.py
@@ -5,6 +5,11 @@ import time as task #time to make the script pose later
 from threading import Thread #threading
 import random #you know random
 
+class Events:
+    def gameover(tail,wall):
+        if not tail or not wall:
+            return True
+
 class snake: #blue print of the snake 
     def __init__(self,termap): #getting the main data here
         self.length = 3
@@ -13,8 +18,15 @@ class snake: #blue print of the snake
         self.y = self.mapsize[0]
         self.x = self.mapsize[1]
         self.movehistory = []
+        self.past_key = "d"
+        self.illegal = {"w":'s',"s":'w',"a":'d',"d":'a'}
     def run(self,key,begin,points): #the main function
-        if key == None or key == "d": #this block is the same for every button wasd
+        if key == None:
+            key = "d"
+        key = str(key).lower()
+        if self.illegal[key] == self.past_key:
+            key = self.past_key
+        if key == 'none' or key == "d": #this block is the same for every button wasd
             if begin != None: #if its the 1st frame we put the snake in the middle
                 self.map[self.y//2,self.x//5] = 2
             indice = np.where(self.map == 2) #1these two blocks to get the snakes head location
@@ -27,9 +39,9 @@ class snake: #blue print of the snake
                 self.map[self.locationY,self.locationX+1] = 2 #updating the map after getting the snake location
             except IndexError as e:
                 pass
+            self.past_key = "d"
 
-
-        elif key == "w":
+        elif key == "w" :
             if begin != None:
                 self.map[y//2,x//5] = 2
             indice = np.where(self.map == 2)
@@ -42,6 +54,7 @@ class snake: #blue print of the snake
                 self.map[self.locationY-1,self.locationX] = 2
             except IndexError as e:
                 pass
+            self.past_key = "w"
 
         elif key == "a":
             if begin != None:
@@ -56,6 +69,7 @@ class snake: #blue print of the snake
                 self.map[self.locationY,self.locationX-1] = 2
             except IndexError as e:
                 pass
+            self.past_key = "a"
 
         elif key =="s":
             if begin != None:
@@ -70,14 +84,20 @@ class snake: #blue print of the snake
                 self.map[self.locationY+1,self.locationX] = 2
             except IndexError as e:
                 pass
+            self.past_key = "s"
+
+        for pos_y,pos_x in self.movehistory: #for every cordination of the tail we put 1 on the map
+            if self.locationY == pos_y and self.locationX == pos_x:
+                return True ,self.map
         self.movehistory.append([self.locationY,self.locationX]) #we saveevery x and y of the head in every frame here so we can add the tail
         if len(self.movehistory) > self.length + points: #if the history has more cordinations than the lenght of the snake than we delet the last cordination
             self.movehistory.pop(0)
         for pos_y,pos_x in self.movehistory: #for every cordination of the tail we put 1 on the map
-            self.map[pos_y,pos_x] = 1  
-                
-        return self.map #we return the value of the final map
-
+            self.map[pos_y,pos_x] = 1
+        if self.locationX == 0 or self.locationX == x or self.locationY == 0 or self.locationY == y:
+            return True ,self.map
+        key = self.past_key
+        return False,np.array(self.map) #we return the value of the final map
 
 
 key = None
@@ -86,21 +106,23 @@ def userinput(): #a loop that we will thread to get the input wasd
         global key
         key = kb.read_key().lower()
 
+events = Events
 begin = True
+hit = False
 inputthread = Thread(target=userinput) # we start the thread here
 inputthread.start()
 x,y = os.get_terminal_size() #we use the os library to get the terminal size
 termap = np.array([[0], #we put 1 x and 1y
                     [0]])
-termap = np.resize(termap,(y,x)) #we change each dimension size by the x and y of the terminal size so we get the terminal map or "termap"
+termap = np.array(np.resize(termap,(y,x))) #we change each dimension size by the x and y of the terminal size so we get the terminal map or "termap"
+
 snake1 = snake(termap) #we put the snake data once
 time = 0.1 #the freshrate
 points = 0 #beggining points dont work now but after we add fruits it should increase
-
 try: #we use try so if the player press contro + c the game stops
     while True:
         frame = [] #the frame that will get printed gets cleared after every ime so we avoid bugs
-        snakemap=snake1.run(key,begin,points) #we runn the snake.run() method in every iteration
+        hit,snakemap=snake1.run(key,begin,points) #we runn the snake.run() method in every iteration
         termap = snakemap #the snake returns the map value and we change our termap to the snake map value i know its not pro move but it works ¯\_(ツ)_/¯
         for row in range(y):
             line = "" #in every line we clear the line hehe
@@ -116,6 +138,9 @@ try: #we use try so if the player press contro + c the game stops
             frame.append(line) #we add every line in the frame
         print('\033[H'+"\n".join(frame),end="") # we print the frame
         begin = None #now we disable the begin since the first iteration is done
-        task.sleep(time) #it waits the times value befor rendering the next frame
+        if hit:
+            print("game over")
+            break
+        task.sleep(time + (points/10)) #it waits the times value befor rendering the next frame
 except KeyboardInterrupt:
     print("quiting") #just print after u press control + c
